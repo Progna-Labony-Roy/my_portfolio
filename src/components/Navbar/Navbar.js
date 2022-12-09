371e07c83f0c85aacf6b283d91bbb50e5cf313ff
Progna_Labony_Roy <labony.progna@gmail.com>
2022-12-10 04:02:16 +0600

    navbar menu items added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import portfolio from '../Image/portfolio.png'
+import React from "react";
+import portfolio from "../Image/portfolio.png";
+import './Navbar.css'
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
-        <a className="navbar-brand" href="#"><img className='logo' src={portfolio} alt='logo...'></img></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            
-          </ul>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+   <div className="container">
+   <a className="navbar-brand" href="#"><img className="logo" src={portfolio} alt='logo'></img></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav ml-auto">
        
-        </div>
-      </nav>
-    );
+        <li className="nav-item">
+          <a className="nav-link" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About me</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Serviecs</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+       
+      </ul>
+    </div>
+   </div>
+  </nav>
+  );
 };
 
 export default Navbar;
