d581a3f7506758cdda6cc9dcf2c64397fdb293cc
Progna_Labony_Roy <labony.progna@gmail.com>
2022-12-10 08:53:53 +0600

    resume download button added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <a
+        href="Progna_Labony_Roy_Resume.pdf"
+        download="Progna_Labony_Roy_Resume.pdf"
+      >
+        <button className="resume-button btn btn-light">Resume</button>
+      </a>
+
+   
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,58 @@
 import React from "react";
 import portfolio from "../Image/portfolio.png";
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-   <div className="container">
-   <a className="navbar-brand" href="#"><img className="logo" src={portfolio} alt='logo'></img></a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav ml-auto">
-       
-        <li className="nav-item">
-          <a className="nav-link" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About me</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Serviecs</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-       
-      </ul>
-    </div>
-   </div>
-  </nav>
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={portfolio} alt="logo"></img>
+        </a>
+        <nav class="navbar navbar-dark bg-dark">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Serviecs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
