f0f5b5810cde79135cb81a9ccb26377b4ad64e4b
Progna_Labony_Roy <labony.progna@gmail.com>
2022-12-11 09:43:42 +0600

    navbar style changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,44 +7,87 @@ import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <div className="header-wrapper">
-        <div className="main-info">
-          <h2 className="text-5xl text-white mb-3 font-semibold">
-            Front-end Developer
-          </h2>
-          <h1 className="text-6xl text-orange-500 font-semibold">
-            Progna Labony Roy
-          </h1>
-          <Typed
-            className="text-white my-3 text-lg"
-            strings={["Web Development", "ReactJS", "Bootstrap", "TailwindCSS"]}
-            typeSpeed={50}
-            backSpeed={60}
-            loop
-          />
-          <br />
-          <a
-            href="Progna_Labony_Roy_Resume.pdf"
-            download="Progna_Labony_Roy_Resume.pdf"
-          >
+    <header>
+      <div className="container header-container">
+        <h5>Hey I'am</h5>
+        <h1>Progna Labony Roy</h1>
+        <Typed
+          className="text-white my-3 text-lg"
+          strings={["Front-end Developer"]}
+          typeSpeed={50}
+          backSpeed={60}
+          loop
+        />
+        <br />
+
+        <br />
+        <a
+          href="Progna_Labony_Roy_Resume.pdf"
+          download="Progna_Labony_Roy_Resume.pdf"
+        >
+          <button className="resume-button btn btn-outline text-white text-lg mr-5">
+            Resume
+          </button>
+          <a href="#contact-id">
             <button className="resume-button btn btn-outline text-white text-lg">
-              Resume
+              Contact
             </button>
           </a>
-          <div>
-            <div>
-              <span className="footer-title">Social</span>
-             <div className="flex">
-              <a className="text-white" href="https://www.linkedin.com/in/progna-labony-roy/"><FaLinkedin className="mr-3"></FaLinkedin></a>
-              <a className="text-white" href="https://www.facebook.com/profile.php?id=100041083752137&mibextid=ZbWKwL"><FaFacebookSquare className="mr-3"></FaFacebookSquare></a>
-              <a className="text-white" href="https://github.com/Progna-Labony-Roy"><FaGithub></FaGithub></a>
-             </div>
-            </div>
-          </div>
+          <br />
+        </a>
+        <div className="mt-5 header-socials">
+          <a            href="https://www.linkedin.com/in/progna-labony-roy/"
+          >
+            <FaLinkedin className="mr-3"></FaLinkedin>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100041083752137&mibextid=ZbWKwL">
+            <FaFacebookSquare className="mr-3"></FaFacebookSquare>
+          </a>
+          <a href="https://github.com/Progna-Labony-Roy">
+            <FaGithub></FaGithub>
+          </a>
         </div>
       </div>
-    </div>
+    </header>
+
+    // <div>
+    //   <div className="header-wrapper">
+    //     <div className="main-info">
+    //       <h2 className="text-5xl text-white mb-3 font-semibold">
+    //         Front-end Developer
+    //       </h2>
+    //       <h1 className="text-6xl text-orange-500 font-semibold">
+    //         Progna Labony Roy
+    //       </h1>
+    //       <Typed
+    //         className="text-white my-3 text-lg"
+    //         strings={["Web Development", "ReactJS", "Bootstrap", "TailwindCSS"]}
+    //         typeSpeed={50}
+    //         backSpeed={60}
+    //         loop
+    //       />
+    //       <br />
+    //       <a
+    //         href="Progna_Labony_Roy_Resume.pdf"
+    //         download="Progna_Labony_Roy_Resume.pdf"
+    //       >
+    //         <button className="resume-button btn btn-outline text-white text-lg">
+    //           Resume
+    //         </button>
+    //       </a>
+    //       <div>
+    //         <div>
+    //           <span className="footer-title">Social</span>
+    //          <div className="flex">
+    //           <a href="https://www.linkedin.com/in/progna-labony-roy/"><FaLinkedin className="mr-3"></FaLinkedin></a>
+    //           <a className="text-white" href="https://www.facebook.com/profile.php?id=100041083752137&mibextid=ZbWKwL"><FaFacebookSquare className="mr-3"></FaFacebookSquare></a>
+    //           <a className="text-white" href="https://github.com/Progna-Labony-Roy"><FaGithub></FaGithub></a>
+    //          </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,36 @@
 import React from "react";
 import portfolio from "../Image/portfolio.png";
 import "./Navbar.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineProject } from "react-icons/ai";
+import { AiOutlineContacts } from "react-icons/ai";
 
 const Navbar = () => {
-
-  const navItems = (
-   <>
-    <li  className="nav-item"><a href="" >Home</a></li>
-    <li  className="nav-item"><a href="#about-id" >About me</a></li>
-    <li  className="nav-item"><a href="#project-id" >Projects</a></li>
-    <li  className="nav-item"><a href="#contact-id">Contact</a></li>
-    </>
-  );
+  // const navItems = (
+  //  <>
+  //   <li  className="nav-item"><a href="" >Home</a></li>
+  //   <li  className="nav-item"><a href="#about-id" >About me</a></li>
+  //   <li  className="nav-item"><a href="#project-id" >Projects</a></li>
+  //   <li  className="nav-item"><a href="#contact-id">Contact</a></li>
+  //   </>
+  // );
 
   return (
-    <div className="sticky navbar bg-neutral text-neutral-content justify-between">
-    <div className="navbar-start">
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
-        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-         {navItems}
-        </ul>
-      </div>
-      <img className="logo" src={portfolio} alt="logo"></img>
+    <div className="navbar">
+      <a href="#" className="active">
+        <AiOutlineHome></AiOutlineHome>
+      </a>
+      <a href="#about-id">
+        <AiOutlineUser></AiOutlineUser>
+      </a>
+      <a href="#project-id">
+        <AiOutlineProject></AiOutlineProject>
+      </a>
+      <a href="#contact-id">
+        <AiOutlineContacts></AiOutlineContacts>
+      </a>
     </div>
-    <div className="navbar-center hidden lg:flex">
-      <ul className="menu menu-horizontal p-0">
-       {navItems}
-      </ul>
-    </div>
-  </div>
   );
 };
 
